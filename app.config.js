180c2d8fcfc46ab9ca4ab3432a0af8773b783e3f
unknown <suhanibadhe@gmail.com>
2025-10-31 02:38:49 +0530

    updated config

--- app.config.js
+++ app.config.js
@@ -4,6 +4,12 @@ export default ({ config }) => ({
   ...config,
   name: config?.name || 'MoveMates',
   slug: config?.slug || 'movemates',
+  
+  // 👇 Add this block (Expo plugin registration)
+  plugins: [
+    'expo-font'
+  ],
+
   extra: {
     ...config?.extra,
     ORS_API_KEY: process.env.ORS_API_KEY,
